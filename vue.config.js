ab75870fdeed4d66d79b71e1e379830977820dea
Dmitrij Tkačenko <dimot9@gmail.com>
2019-08-21 15:08:17 +0200

    :bug: Add SSR support

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,5 @@
 module.exports = {
   lintOnSave: false,
-  chainWebpack: (config) => {
-    // remove the old loader
-    config.module.rules.delete('svg');
-  },
-  configureWebpack: {
-    module: {
-      rules: [{
-        test: /\.(svg)(\?.*)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-            },
-          },
-        ],
-      }],
-    },
-  },
   css: {
     extract: false,
   },
