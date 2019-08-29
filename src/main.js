a22e770a8027f7854008acc04b7c11849296f937
Dmitrij Tkačenko <dimot9@gmail.com>
2019-08-29 11:05:43 +0200

    :zap: Import just used Buefy components

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { Table, Icon, Button } from 'buefy'
-import Buefy from 'buefy'
+import { Field, Checkbox, Switch, Select, Numberinput, Input, Loading, Button, Icon } from 'buefy'
 import 'buefy/dist/buefy.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false;
 
-Vue.use(Table)
-Vue.use(Icon)
+Vue.use(Field)
+Vue.use(Checkbox)
 Vue.use(Button)
-Vue.use(Buefy, {
-  defaultIconPack: 'mdi'
-})
+Vue.use(Switch)
+Vue.use(Select)
+Vue.use(Numberinput)
+Vue.use(Input)
+Vue.use(Loading)
+Vue.use(Icon)
 
 new Vue({
   render: h => h(App),
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  lintOnSave: false,
-  css: {
-    extract: false,
-  },
-};
